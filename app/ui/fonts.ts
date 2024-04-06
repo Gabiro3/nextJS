355375903b5ea80da4c20adf2546c37b0e403232
oleonpublishers <33125607-oleonpublishers@users.noreply.replit.com>
2024-04-06 07:32:04 +0000

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Inter, Poppins, Lusitana } from 'next/font/google';
+export const poppins = Poppins({ subsets: ['latin-ext'], weight: '200'});
+export const lusitana = Lusitana({ subsets: ['latin'], weight:'400'});
